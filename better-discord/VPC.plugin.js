de5b57d3f47d8ab9da1c5f033d768a23f64860e2
Venodez <30352734+Venodez@users.noreply.github.com>
2021-05-18 15:56:00 +0200

    Changement du nom du fichier

---
+++ better-discord/VPC.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name VPC
+ * @author Venodez
+ * @version 1.0
+ * @description Permet de crée un tchat de proximité sur Minecraft à partir de Discord.
+ * @website https://github.com/Venodez/VPC
+ * @source https://github.com/Venodez/VPC/tree/main/better-discord/
+ * @updateUrl https://github.com/Venodez/VPC/blob/main/better-discord/VPC.js
+ */
