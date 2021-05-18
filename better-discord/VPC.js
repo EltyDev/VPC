ef7851167febf988be19db2263ef91ee03127bb8
Venodez <30352734+Venodez@users.noreply.github.com>
2021-05-18 15:51:13 +0200

    Ajout du plugin BTTDiscord

---
+++ better-discord/VPC.js
@@ -0,0 +1,13 @@
+/**
+ * @name VPC
+ * @author Venodez
+ * @authorId 278543574059057154
+ * @version 1.0
+ * @description Allows you to enter larger Messages, which will automatically split into several smaller Messages
+ * @invite Jx3TjNS
+ * @donate https://www.paypal.me/MircoWittrien
+ * @patreon https://www.patreon.com/MircoWittrien
+ * @website https://mwittrien.github.io/
+ * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/SendLargeMessages/
+ * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Plugins/SendLargeMessages/SendLargeMessages.plugin.js
+ */
